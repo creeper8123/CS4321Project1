d817223b35114c053a7f0620cd9b7e59ebe730b6
creeper8123 <anthgb@icloud.com>
2023-10-23 17:21:53 -0400

    Changed stuff for problem 1

--- prob1.c
+++ prob1.c
@@ -8,28 +8,41 @@
 #include "prob1.h"
 
 int main(int argc, char** argv){
-    FILE* file = fopen("prob1out.csv", "w");
 
-    //Confirm file exists
-    if(file == NULL){
-        printf("Error opening file!\n");
-        exit(1);
+    if(argc > 1){ //Running test cases
+        runTestCases();
+        exit(0);
     }
 
-    //Add header
-    fprintf(file, "Iteration Number,n=10,n=20,n=30,n=40,n=50,n=60,n=70,n=80,n=90,n=100,\n");
+    //Not running test cases
 
     //Calculate important values
     int upper = 16000;
     int lower = 1600;
-    int* array = malloc(sizeof(int) * 100);
+    int* array = malloc(sizeof(int) * upper);
+
+
+
+
+    //-----------LOWER M (1600)----------------
+    FILE* file = fopen("prob1outLower.csv", "w");
+
+    //Confirm file exists
+    if(file == NULL){
+        printf("Error opening lower file!\n");
+        exit(1);
+    }
+
+    //Add header to file
+    fprintf(file, "Iteration Number,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,\n",
+    (lower/10) * 1, (lower/10) * 2, (lower/10) * 3, (lower/10) * 4, (lower/10) * 5, (lower/10) * 6, (lower/10) * 7, (lower/10) * 8, (lower/10) * 9, (lower/10) * 10);
 
     //Repeat for many tests
     for (int i = 1; i <= NUM_OF_ITERATIONS; i++){
         fprintf(file, "%d,", i); //add iteration number
 
         //Repeat for different array sizes
-        for (int j = 10; j <= 100; j+=10){
+        for (int j = lower/10; j <= lower; j+=lower/10){
 
             //Fill array with new values
             for (int k = 0; k < j; k++){
@@ -37,40 +50,134 @@ int main(int argc, char** argv){
             }
 
             //Add data to file
-            fprintf(file, "%d,", uniqueElementAlg(array, j));
+            int matchFound = 0;
+            fprintf(file, "%d,", uniqueElementAlg(array, j, &matchFound));
+        }
+        fprintf(file, "\n");
+    }
+
+    //Close file
+    fclose(file);
+
+    printf("Data written to prob1outLower.csv\n");
+    //-------------END OF LOWER-----------
+
+
+    //------------------UPPER M (16000)-----------
+    file = fopen("prob1outUpper.csv", "w");
+
+    //Confirm file exists
+    if(file == NULL){
+        printf("Error opening upper file!\n");
+        exit(1);
+    }
+
+    //Add header to file
+    fprintf(file, "Iteration Number,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,n=%d,\n",
+    (upper/10) * 1, (upper/10) * 2, (upper/10) * 3, (upper/10) * 4, (upper/10) * 5, (upper/10) * 6, (upper/10) * 7, (upper/10) * 8, (upper/10) * 9, (upper/10) * 10);
+
+    //Repeat for many tests
+    for (int i = 1; i <= NUM_OF_ITERATIONS; i++){
+        fprintf(file, "%d,", i); //add iteration number
+
+        //Repeat for different array sizes
+        for (int j = upper/10; j <= upper; j+=upper/10){
+
+            //Fill array with new values
+            for (int k = 0; k < j; k++){
+                array[k] = (rand() % (upper - lower + 1)) + lower;
+            }
+
+            //Add data to file
+            int matchFound = 0;
+            fprintf(file, "%d,", uniqueElementAlg(array, j, &matchFound));
         }
         fprintf(file, "\n");
     }
     
+    //close file
+    fclose(file);
+
     //Clean up
     free(array);
-    fclose(file);
+
+    printf("Data written to prob1outUpper.csv\n");
 
     return 0;
 }
 
-int uniqueElementAlg(int* array, int arraySize){
-    int numOfCollisions = 0;
+int uniqueElementAlg(int* array, int arraySize, int* matchFound){
+    int numOfComparisons = 0;
 
     for (int i = 0; i < arraySize-1; i++){
         for (int j = i+1; j < arraySize; j++){
+            numOfComparisons++;
             if(array[i] == array[j]){
-                numOfCollisions++;
+                *matchFound = 1;
+                return numOfComparisons;
             }
         }
     }
 
-    return numOfCollisions;
+    *matchFound = 0;
+    return numOfComparisons;
 }
 
-/*
-Alg directly from notes:
+void runTestCases(){
+    printf("argc > 1, running problem 1 test results: (0 = failure)\n");
 
-for(i from 0 to n-2){ //if n-1, would compare with itself
-    for(j from i+1 to n-1){
-        if(A[i] == A[j]){
-            return false;
-        }
+    //Setup arrays
+    int* testData = (int*) malloc(10 * sizeof(int));
+    int expectedResult = 0;
+    int actualResult = 0;
+
+    //TEST 1
+    //All elements unique
+    expectedResult = 0;
+    for (int i = 0; i < 10; i++){
+        testData[i] = i;
     }
+    uniqueElementAlg(testData, 10, &actualResult);
+    printf("Test 1 result: %d\n", expectedResult == actualResult);
+
+    //TEST 2
+    //[0] == [1]
+    expectedResult = 1;
+    for (int i = 0; i < 10; i++){
+        testData[i] = i;
+    }
+    testData[1] = 0;
+    uniqueElementAlg(testData, 10, &actualResult);
+    printf("Test 2 result: %d\n", expectedResult == actualResult);
+
+    //TEST 3
+    //[0] == [9]
+    expectedResult = 1;
+    for (int i = 0; i < 10; i++){
+        testData[i] = i;
+    }
+    testData[9] = 0;
+    uniqueElementAlg(testData, 10, &actualResult);
+    printf("Test 3 result: %d\n", expectedResult == actualResult);
+
+    //TEST 4
+    //[8] == [9]
+    expectedResult = 1;
+    for (int i = 0; i < 10; i++){
+        testData[i] = i;
+    }
+    testData[9] = 8;
+    uniqueElementAlg(testData, 10, &actualResult);
+    printf("Test 4 result: %d\n", expectedResult == actualResult);
+
+    //TEST 5
+    //All values are the same
+    expectedResult = 1;
+    for (int i = 0; i < 10; i++){
+        testData[i] = 3;
+    }
+    uniqueElementAlg(testData, 10, &actualResult);
+    printf("Test 5 result: %d\n", expectedResult == actualResult);
+
+    free(testData);
 }
-*/
